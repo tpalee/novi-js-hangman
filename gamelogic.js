b117ca0d2e479c4968e06be4ebc33d0986f793fe
tpalee <thieulee@hotmail.com>
2021-07-09 12:49:55 +0200

    add extra comments in feature/galgje-uitwerkin

--- gamelogic.js
+++ gamelogic.js
@@ -1,41 +1,40 @@
 function displayWordSoFar(word, guesses) {
-    //maak een array aan om de resultaten van de check in op te slaan
+    //  maak een array aan om de resultaten van de check in op te slaan
     let resultArray = [];
-    //boolean geeft aan of er een letter gevonden is,deze begint als false
+    //  boolean geeft aan of er een letter gevonden is,deze begint als false
     let found = false;
-    //for loop loopt over elke letter van het "gezochte"woord (word)
+    //  for loop loopt over elke letter van het "gezochte"woord (word)
     for (let i = 0; i < word.length; i++) {
-        //for loop loopt over de guesses array en vergelijkt elke letter met elke letter van word
+        //  for loop loopt over de guesses array en vergelijkt elke letter met elke letter van word
         for (let j = 0; j < guesses.length; j++) {
-            //als letter een match is,verander found in true; en plaatst de letter in de resultArray op de juiste index,daarna stopt deze loop;
+            //  als letter een match is,verander found in true; en plaatst de letter in de resultArray op de juiste index,daarna stopt deze loop;
             if (guesses[j] == word.charAt(i)) {
                 found = true;
                 resultArray.push(word.charAt(i) + ' ')
                 break;
-                //niks gevonden? found is false,door met de volgende letter uit de guesses-array
+                // niks gevonden? found is false,door met de volgende letter uit de guesses-array
             } else {
 
                 found = false;
             }
         }
-        //als er na alle letters uit de gues-array geen match is push dan een underscore met spatie in de resultArray
+        //  als er na alle letters uit de gues-array geen match is push dan een underscore met spatie in de resultArray
         if (!found) {
-            resultArray.push('_ ')
+            resultArray.push('_ ');
         }
     }
-    //na het itereren return je de reultarray omgezet naar een string
-    //console.log(resultArray.join(""))
+    // na het itereren return je de resultarray omgezet naar een string
     return resultArray.join("");
 }
 
 
 function isGameWon(word, guesses) {
-    //log uitkomst van displayWordSoFar iin de console
+    // log uitkomst van displayWordSoFar in de console
     const logResult=displayWordSoFar(word, guesses);
     console.log(logResult);
-    //result is het teruggeven resultaat van de functie 'displayWordSoFar', waarbij alle spaties worden verwijderd.
+    // result is het teruggeven resultaat van de functie 'displayWordSoFar', waarbij alle spaties worden verwijderd.
     const result = displayWordSoFar(word, guesses).replace(/\s/g, '');
-    //vergelijk word met result, gelijk? return true(gewonnen), anders return false(niet gewonnen);
+    //  vergelijk word met result, gelijk? return true(gewonnen), anders return false(niet gewonnen);
     if (word == result) {
         console.log("Je hebt gewonnen!!!")
         return true;
@@ -47,23 +46,23 @@ function isGameWon(word, guesses) {
 }
 
 function isGameLost(word, guesses) {
-    //result is het teruggeven resultaat van de functie 'displayWordSoFar', waarbij alle spaties worden verwijderd.
+    //  result is het teruggeven resultaat van de functie 'displayWordSoFar', waarbij alle spaties worden verwijderd.
     const result = displayWordSoFar(word, guesses).replace(/\s/g, '');
-    //counter houdt het aantal juiste letters bij;
+    //  counter houdt het aantal juiste letters bij;
     let goodCounter = 0;
-    //loop door het aantal letters in de guessesarray
+    //  loop door het aantal letters in de guessesarray
     for (let i = 0; i < guesses.length; i++) {
-        //loop met elke letter uit guesses door het woord en check of er een match is
+        //  loop met elke letter uit guesses door het woord en check of er een match is
         for (let j = 0; j < word.length; j++) {
             if (guesses[i] == word.charAt(j)) {
-                //match? verhoog de counter met 1 en stop deze loop
+                //  match? verhoog de counter met 1 en stop deze loop
                 goodCounter += 1;
                 break;
             }
         }
 
     }
-    //als het aantal ingevoerde letters min de juiste letters groter of gelijk is aan 7 en word niet gelijk is aan het resultaat return true(game verloren)
+    //  als het aantal ingevoerde letters min de juiste letters groter of gelijk is aan 7 en word niet gelijk is aan het resultaat return true(game verloren)
     if (((guesses.length - goodCounter) >= 7) && !(word == result)) {
         console.log("Helaas, je hebt verloren...");
         return true;
@@ -73,6 +72,8 @@ function isGameLost(word, guesses) {
 }
 
 
+
+//  functie met switch statement tekent de galg aan de hand van het aantal fouten(counter)
 function showHangman(counter){
     switch(counter) {
         case 1:
--- index.js
+++ index.js
@@ -1,36 +1,50 @@
 const { question } = require("readline-sync");
 const { displayWordSoFar, isGameWon, isGameLost, showHangman } = require("./gamelogic");
+//  countFaults geeft aan hoeveel foute antwoorden zijn gegegeven
 let countFaults=0;
 function game(word, guesses) {
   console.log("Dit heb je tot nu toe geraden: ", guesses);
+
+  //  check of game al gewonnen is
   isGameWon(word,guesses);
+
+  //  check of game al verloren is
   isGameLost(word,guesses);
+
+  //  check of ingevoerde letter wel/niet voorkomt in het woorden geeft string met juist geradeletters en nog niet gerade letters(streepjes)
   displayWordSoFar(word,guesses);
+
+  // deze variable bevat de waarde van displayWordSoFar voor de nieuwe input
   let tempResult=displayWordSoFar(word,guesses);
 
+  //  input wordt automatisch omgezet naar lowerCase
   const letter = question("Raad een letter: ").toLowerCase();
-if(letter.length==1){
-  if(!guesses.includes(letter)){
-    guesses.push(letter);
-    console.log("Letter is not in array");
-    if(tempResult==displayWordSoFar(word,guesses)){
-      countFaults+=1;
+
+  //  check of de invoer uit 1 karakter bestaat
+  if (letter.length == 1) {
+    //  check of ingevoerde letter niet voorkomt in de guesses-array
+    if (!guesses.includes(letter)) {
+      guesses.push(letter);
+      //  check of tempResult gelijk is aan displayWordSoFar, zo ja is er geen nieuwe letter goed geraden, dus faultCounter +1
+      if (tempResult == displayWordSoFar(word, guesses)) {
+        countFaults += 1;
+      }
+    } else {
+      console.log('Helaas, dit karakter heb je al eerder ingevoerd!');
     }
+
+    //  functie die de galg tekent in de console;
+    showHangman(countFaults);
+    console.log(`Foute antwoorden: ${countFaults}`);
+  } else {
+    console.log("Sorry, je mag maar 1 karakter tegelijk invoeren");
   }
-  else{
-    console.log('letter is already in array')}
-
-  showHangman(countFaults);
-  console.log(`Je hebt ${countFaults} foute antwoorden gegeven..`)
-}
-else{
-  console.log("Sorry, je mag maar 1 karakter tegelijk invoeren")}
 
 
 
 
 
-
+  //  als game gewonnen of verloren is, stop de game
   if((isGameWon(word,guesses))||(isGameLost(word,guesses))){
     return;
   }
